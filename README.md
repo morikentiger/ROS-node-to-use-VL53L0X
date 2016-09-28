28a35bb42fef84649f20ec9a9deb9bce76e7bcb6
morikentiger <kenrur339@gmail.com>
2016-09-28 16:39:23 +0900

    came to be able to use vl53l0x by Arduino library

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # ROS-node-to-use-VL53L0X-in-python
-This repository is ROS node to use the VL53L0X written in python.
+This repository is ROS node to use the VL53L0X written in C++(not python).
 
 !!! N O W  C O D I N G !!!
 
 
-python code for ROS node 
+//python code for ROS node 
+C++ code for ROS node
 
 in reference to the pololu's repository https://github.com/pololu/vl53l0x-arduino (Use code:single.ino,VL53L0X.cpp,VL53L0X.h) 
 
-use "smbus" module instead of "Wire.h" (Arduino I2C Library)
+//use "smbus" module instead of "Wire.h"(Arduino I2C Library)
+use "i2c-dev.h" header instead of "Wire.h"(Arduino I2C Library)
+
+//directrory "src" is only test
+directory "script" is only test
+
+//"vl53l0x_test.cpp" is Not used for ROS node
 
 
-in "src" is only test
+//"single.py" in "script" is (test) node
 
-"vl53l0x_test.cpp" is Not used for ROS node
-
-
-"single.py" in "script" is (test) node
+"Single.cpp" in "src" is main code for node
--- src/Single.cpp
+++ src/Single.cpp
@@ -6,9 +6,14 @@
  "SingleRanging" examples in the VL53L0X API.
 
  The range readings are in units of mm. */
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
-#include <Wire.h>
-#include <VL53L0X.h>
+#include <stdio.h>
+#include <stddef.h>	//#define NULL ...
+#include <linux/i2c-dev.h>
+#include <test_vl53l0x/VL53L0X.h>
+
 
 VL53L0X sensor;
 
@@ -33,33 +38,96 @@ VL53L0X sensor;
 
 void setup()
 {
-  Serial.begin(9600);
-  Wire.begin();
-
+  //Serial.begin(9600);
+  //Wire.begin();
+  
+	//ROS_INFO("testsetup");
   sensor.init();
+  //ROS_INFO("testsetup2");
   sensor.setTimeout(500);
+  //sensor.setTimeout(2000);
 
 #if defined LONG_RANGE
   // lower the return signal rate limit (default is 0.25 MCPS)
+  //ROS_INFO("testsetup3");
   sensor.setSignalRateLimit(0.1);
+//  sensor.setSignalRateLimit(0.25);
   // increase laser pulse periods (defaults are 14 and 10 PCLKs)
-  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
-  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+  //ROS_INFO("testsetup4");
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);//18);
+  //ROS_INFO("testsetup5");
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);//14);
 #endif
 
 #if defined HIGH_SPEED
   // reduce timing budget to 20 ms (default is about 33 ms)
+  //ROS_INFO("testsetup6");
   sensor.setMeasurementTimingBudget(20000);
 #elif defined HIGH_ACCURACY
   // increase timing budget to 200 ms
+  //ROS_INFO("testsetup7");
   sensor.setMeasurementTimingBudget(200000);
 #endif
 }
 
 void loop()
 {
-  Serial.print(sensor.readRangeSingleMillimeters());
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  //ROS_INFO("testloop");
+  //ROS_INFO(sensor.readRangeSingleMillimeters());
+  //if (sensor.timeoutOccurred()) { ROS_INFO(" TIMEOUT"); }
+  printf("readRangeSingleMillimeters:%d\n",sensor.readRangeSingleMillimeters());
+  if (sensor.timeoutOccurred()) { printf(" TIMEOUT_loop\n"); }
+  //ROS_INFO("testloopend");
 
-  Serial.println();
+}
+
+int main(int argc, char **argv)
+{
+	////ROS_INFO("test0");	
+	ros::init(argc, argv, "Single_VL53L0X");
+	ros::NodeHandle n;
+	printf("test\n");
+	//ROS_INFO("test");
+	setup();
+	
+	//ROS_INFO("test2");
+	
+	//ROS_INFO("test3");
+	
+	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+	//ROS_INFO("test4");
+	ros::Rate loop_rate(10);
+
+	//ROS_INFO("test5");
+	int count = 0;
+	while (ros::ok())
+	{
+		//ROS_INFO("test6");
+		std_msgs::String msg;
+
+		std::stringstream ss;
+		ss << "hello world " << count;
+		msg.data = ss.str();
+
+		//ROS_INFO("%s", msg.data.c_str());
+
+		chatter_pub.publish(msg);
+
+		//ROS_INFO("test7");
+		ros::spinOnce();
+
+		//ROS_INFO("test8");
+		loop();
+
+		//ROS_INFO("test9");
+		loop_rate.sleep();
+		++count;
+	}
+
+	//ROS_INFO("testEnd");
+	//ROS_INFO("testEnd");
+	printf("testEnd\n");
+	return 0;
+
 }
